f5f7d86186b449e71a79949caef67e1f0353f292
Morema <25913934+Mo-rema07@users.noreply.github.com>
2018-07-31 14:33:01 +0400

    Next and Prev Buttons work

---
+++ js/countries.js
@@ -0,0 +1,10 @@
+function loadEnrol(){
+    let country;
+    $(".btnEnrolStats").click(function () {
+        country= $(this).parent().attr('id').toLowerCase();
+        indicator=$(this).text().split(" ").join("");
+        window.location = "enrolment.html?country="+country+"&indicator="+indicator;
+    });
+
+}
+loadEnrol();
--- js/enrolment.js
+++ js/enrolment.js
@@ -1,10 +1,120 @@
-function loadEnrol(){
-    var country;
-    $(".btnEnrolStats").click(function () {
-        country= $(this).parent().attr('id').toLowerCase();
-        indicator=$(this).text().split(" ").join();
-        window.location = "enrolment.html?country="+country+"&indicator="+indicator;
-    });
+Chart.defaults.global.defaultFontColor = '#555';
+Chart.defaults.global.defaultFontFamily = "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
+Chart.defaults.global.defaultFontSize = 16;
+Chart.scaleService.updateScaleDefaults('bar', {
+    ticks: {
+        min: 0
+    }
+});
+const country_codes = {
+    "lesotho": "LSO",
+    "mauritius": "MUS",
+    "uk":"GBR"
+};
+indicators =[{id: "AllProgrammesofStudy",code:"SE.TER.ENRR", text:"All Programmes of Study"},
+    {id:"Agriculture", code: "UIS.FOSEP.56.F600", text:"Agriculture"},
+    {id:"Education",code:"UIS.FOSEP.56.F140",text:"Education"},
+    {id:"Engineering,ManufacturingandConstruction",text:"Engineering, Manufacturing and Construction",code:"UIS.FOSEP.56.F500"},
+    {id:"HealthandWelfare",text:"Health and Welfare",code:"UIS.FOSEP.56.F700"},
+    {id:"HumanitiesandArts",text:"Humanities and Arts",code:"UIS.FOSEP.56.F200"},
+    {id:"UnspecifiedFields",text:"Unspecified Fields",code:"UIS.FOSEP.56.FUK"},
+    {id:"Science",text:"Science",code:"UIS.FOSEP.56.F400"},
+    {id:"Services",text:"Services",code:"UIS.FOSEP.56.F800"},
+    {id:"SocialSciences,BusinessandLaw",text:"Social Sciences, Business and Law",code:"UIS.FOSEP.56.F300"}]
+;
+let length = indicators.length;
+console.log(length);
+let indIndex =0;
+const url = window.location.href;
+const index = url.indexOf("?");
+let params = url.substring(index + 1).split("&");
+let param = params[0];
+let country = param.substring(param.indexOf("country=")+8);
+const code = country_codes[country];
+param=params[1];
+country = param.substring(param.indexOf("indicator=")+10);
+let indicator;
+for (let i=length-1; i>-1; i--){
+    if(country===indicators[i].id){
+        indicator = indicators[i].code;
+        indIndex=i;
+    }
+}
+let apiResponse;
+function loadDoc(code,indicator) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            apiResponse = JSON.parse(xhttp.responseText);
+            var data = apiResponse[1];
+            console.log(data);
+            var year =[];
+            var percentage=[];
+
+            for (var i=0; i<data.length; i++){
+                year.push(data[i].date);
+                percentage.push(data[i].value);
+            }
+            var myChart =document.getElementById("myChart").getContext('2d');
+            var myVisual = new Chart(myChart, {
+                type: 'bar',
+                data: {
+                    labels:year,
+                    datasets: [{
+                        label: "%",
+                        backgroundColor: '#0075C2',
+                        borderColor: "#0075C2",
+                        data: percentage,
+                    }]
+                },
+
+                // Configuration options go here
+                options: {
+                    title: {
+                        display: true,
+                        text: data[0].indicator.value
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                min: 0
+                            }
+                        }]
+                    }
+                }
+            });
+
+        }
+    };
+    xhttp.open("GET", "http://api.worldbank.org/v2/countries/"+code+"/indicators/"+indicator+"?date=2009:2015&format=json", true);
+    xhttp.send();
 
 }
-loadEnrol();
+loadDoc(code,indicator);
+// loadData(code, indicator);
+
+function loadNext(){
+    $("#enrolNext").click(function () {
+        if (indIndex < length-1) {
+            console.log(indIndex);
+            indIndex += 1;
+            indicator = indicators[indIndex].code;
+            console.log(indicator);
+            loadDoc(code, indicator);
+        }
+    });
+}
+loadNext();
+
+function loadPrev(){
+    $("#enrolPrev").click(function () {
+        if (indIndex > 0) {
+            console.log(indIndex);
+            indIndex -= 1;
+            indicator = indicators[indIndex].code;
+            console.log(indicator);
+            loadDoc(code, indicator);
+        }
+    });
+}
+loadPrev();
